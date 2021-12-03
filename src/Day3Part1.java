caf025704cbb9e29704af37695b98dcf10627b0c
BertTheTurtl <92934395+BertTheTurtl@users.noreply.github.com>
2021-12-03 00:40:11 -0500

    Setting up Day 3 for later today

---
+++ src/Day3Part1.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Day3Part1
+{
+    public static void main(String[] args) throws IOException
+    {
+        List<String> file = Files.readAllLines(Path.of("inputDay3.txt"));
+    
+        for (String s : file)
+        {
+
+        }
+    }
+}
