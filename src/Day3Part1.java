c3e986159347d845178bc547fcf7f181d5403eb6
BertTheTurtl <92934395+BertTheTurtl@users.noreply.github.com>
2021-12-07 23:37:46 -0500

    Wow, I didn't even make it to day 3

--- src/Day3Part1.java
+++ src/Day3Part1.java
@@ -8,9 +8,17 @@ public class Day3Part1
     public static void main(String[] args) throws IOException
     {
         List<String> file = Files.readAllLines(Path.of("inputDay3.txt"));
-    
+
+        int countOne = 0;
+        int countZero = 0;
+
         for (String s : file)
         {
+            int n = Integer.parseInt(s);
+            if (n % 100000000000 == 1)
+                countOne++;
+            else
+                countZero++;
 
         }
     }
